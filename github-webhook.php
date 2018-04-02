8638bf434585752d9529d9e20a7a737f2d2c929c
hachibu <raymondsohn@gmail.com>
2018-04-02 13:32:09 -0700

    Add logging to github-webhook.php script
    Add logging to github-webhook.php script.
    
    Minor:
    - Add logs directory to gitignore.

--- github-webhook.php
+++ github-webhook.php
@@ -6,24 +6,43 @@ use GitHubWebhook\Handler;
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
+function write_log($message) {
+  $log_directory = 'logs';
+  $file_path = $log_directory . '/github-webhook.log';
+
+  if (!is_dir($log_directory)) {
+    mkdir($log_directory, 0755);
+  }
+
+  if (!is_file($file_path)) {
+    $file_handle = fopen($file_path, 'w');
+    fclose($file_handle);
+  }
+
+  $message = gmdate('[Y-m-d H:i:s]') . ' ' . $message . "\n";
+
+  echo($message);
+  error_log($message, 3, $file_path);
+}
+
 $gh_webhook_secret = getenv('GH_WEBHOOK_SECRET');
 
 if (empty($gh_webhook_secret)) {
-  echo('GH_WEBHOOK_SECRET environment variable not set' . "\n");
+  write_log('GH_WEBHOOK_SECRET environment variable not set');
   exit(1);
 }
 
 $git_path = `which git`;
 
 if (empty($git_path)) {
-  echo('git not found on path' . "\n");
+  write_log('git not found on path');
   exit(1);
 }
 
 $git_branch = trim(shell_exec('git rev-parse --abbrev-ref HEAD'));
 
 if ($git_branch != 'master') {
-  echo('git branch is not master: ' . $git_branch . "\n");
+  write_log('git branch is not master: ' . $git_branch);
   exit(1);
 }
 
@@ -33,10 +52,10 @@ if ($request->validate()) {
   $event = $request->getEvent();
 
   if ($event == 'push') {
-    echo(shell_exec('git pull --rebase'));
+    write_log(shell_exec('git pull --rebase'));
   }
 } else {
-  echo('request is invalid' . "\n");
+  write_log('request is invalid');
   exit(1);
 }
 ?>
