001b6632b09418d1e9558df874513237b4c0c942
hachibu <raymondsohn@gmail.com>
2018-04-08 19:15:01 -0700

    Comment out comments from theme
    Comment out comments from theme.

--- wp-content/themes/twentysixteen/single.php
+++ wp-content/themes/twentysixteen/single.php
@@ -19,9 +19,9 @@ get_header(); ?>
 			get_template_part( 'template-parts/content', 'single' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
+			// if ( comments_open() || get_comments_number() ) {
+			// 	comments_template();
+			// }
 
 			if ( is_singular( 'attachment' ) ) {
 				// Parent post navigation.
